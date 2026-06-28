906330006ce46a8b8ff4a01274e79cbd45959203
Sankalp Panchabhai <sankalppanchabhai5@gmail.com>
2026-06-28 10:23:33 +0530

    remove unwanted folders

--- server/dist/middleware/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = asyncHandler;
-function asyncHandler(handler) {
-    return (request, response, next) => {
-        Promise.resolve(handler(request, response, next)).catch(next);
-    };
-}
--- server/src/middleware/asyncHandler.ts
+++
@@ -1,7 +0,0 @@
-import type { NextFunction, Request, RequestHandler, Response } from 'express';
-
-export function asyncHandler(handler: (request: Request, response: Response, next: NextFunction) => Promise<void> | void): RequestHandler {
-    return (request, response, next) => {
-        Promise.resolve(handler(request, response, next)).catch(next);
-    };
-}
